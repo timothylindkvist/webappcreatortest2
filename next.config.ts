a611496e3a3b5730f7bdc171e62c7b16d1bda947
timothylindkvist <timothy.lindkvist@gmail.com>
2025-09-13 00:45:51 +0200

    a

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/i,
+      use: 'raw-loader',
+    })
+    return config
+  },
 }
 
 export default nextConfig
